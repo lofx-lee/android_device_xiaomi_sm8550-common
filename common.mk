1abd134672592fae6c84f8cf9fe2f8698f8581fc
leo <mutongbaobao@gmail.com>
2026-04-17 11:57:05 +0800

    sm8550-common: Inherit Dolby Atmos
    Change-Id: Ifa5011906b69d92175e2e86948580c47eee520a9

--- common.mk
+++ common.mk
@@ -13,6 +13,9 @@ $(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
 # A/B
 $(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/launch_with_vendor_ramdisk.mk)
 
+# Inherit DolbyAtmos
+$(call inherit-product, device/xiaomi/sm8550-common-dolby/dolby.mk)
+
 AB_OTA_POSTINSTALL_CONFIG += \
     RUN_POSTINSTALL_system=true \
     POSTINSTALL_PATH_system=system/bin/otapreopt_script \
